afc0e6916449298de9be82c1e9a3475267e53c39
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-07-19 22:04:30 -0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <h3>hey!</h3>;
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
