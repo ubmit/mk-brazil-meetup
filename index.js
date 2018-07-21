4ea7eec1d5b1ddd36e846bf92a3ffdd7e5a1a0a2
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-07-21 18:20:16 -0300

    api requests are working

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
+const routes = require('./routes');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api/keyboards', routes);
 
-app.get('/', (req, res) => res.send('clack!'));
-
-app.listen(process.env.PORT || 5000);
+const port = process.env.PORT || 5000;
+app.listen(port);
---
+++ routes/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require('mongoose');
+require('../models/Keyboard');
+const Keyboard = mongoose.model('keyboards');
+
+router.get('/', (req, res) => {
+  Keyboard.find({}, (err, keyboards) => {
+    res.json(keyboards);
+  });
+});
+
+router.post('/', (req, res) => {
+  let keyboard = new Keyboard(req.body);
+  keyboard.save();
+  res.send(keyboard);
+});
+
+router.get('/:keyboardId', (req, res) =>
+  Keyboard.findById(req.params.keyboardId, (err, keyboard) => {
+    res.json(keyboard);
+  })
+);
+
+router.patch('/:keyboardId', (req, res) =>
+  Keyboard.findById(req.params.keyboardId, (err, keyboard) => {
+    if (req.body._id) {
+      delete req.body._id;
+    }
+    for (e in req.body) {
+      keyboard[e] = req.body[e];
+    }
+    keyboard.save();
+    res.json(keyboard);
+  })
+);
+
+router.delete('/:keyboardId', (req, res) =>
+  Keyboard.findById(req.params.keyboardId, (err, keyboard) => {
+    keyboard.remove();
+    res.send(keyboard);
+  })
+);
+
+module.exports = router;
