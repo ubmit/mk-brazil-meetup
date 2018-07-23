9fe4429482d64ef32841a69c43e54a18a65a77b3
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-07-23 18:55:16 -0300

    preparing for heroku deployment

--- index.js
+++ index.js
@@ -10,5 +10,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/keyboards', routes);
 
+if (process.env.NODE_ENV === 'production') {
+  // express will serve up production assets
+  // like our main.js file
+  app.use(express.static('./client/build'));
+
+  // express will serve the index.html file if
+  // it doesnt recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port);
