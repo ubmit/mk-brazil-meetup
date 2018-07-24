e7eddf4de93ff586a05ecd41e587deef5c1b382d
Guilherme de Andrade <guilhermedeandrade@me.com>
2018-07-23 21:37:40 -0300

    centered modal

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Menu, Icon } from 'semantic-ui-react';
 
-const styles = {
-  header: {
-    marginBottom: 20
-  }
+const marginHeader = {
+  marginBottom: 20
 };
 
 const Header = () => {
   return (
-    <Menu widths={1} style={styles.header}>
+    <Menu widths={1} style={marginHeader}>
       <Menu.Item name="meetup">
         <Icon name="keyboard" /> Mechanical Keyboards Brazil - #1 Meetup
       </Menu.Item>
--- client/src/components/KeyboardCard.js
+++ client/src/components/KeyboardCard.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Card, Icon, Image, Modal, Button } from 'semantic-ui-react';
+import { Card, Icon, Image, Modal } from 'semantic-ui-react';
+
+const centerModal = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+};
 
 const KeyboardCard = props => {
   const { image, name, description, owner } = props;
   return (
     <Card>
-      <Modal trigger={<Image src={image} />} basic size="small">
+      <Modal
+        trigger={<Image src={image} />}
+        basic
+        size="small"
+        style={centerModal}
+      >
         <Modal.Content>
           <Image src={image} />
         </Modal.Content>
